264e227d99d428fb031633bd3fc4e56e239f549a
mihaelasandrea <mihaelasandrea@yahoo.com>
2021-03-09 18:03:35 +0000

    Fix page headers, add copyright script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
+/* Autoclose navbar when click on link */
+
 $('.navbar-collapse a').click(function(){
     $(".navbar-collapse").collapse('hide');
 });
+
+/* Copyright */
+
+function copyrightYear() {
+    var d = new Date();
+    var y = d.getFullYear();
+    document.getElementById("copyright").innerHTML = y;
+}
+
+copyrightYear();
